fa01c4540007b41b7a70a516aebd6d94a375c8d5
Daniel <danielmarreirosdeoliveira@googlemail.com>
2016-12-13 15:13:00 +0100

    Rename method.

--- src/main/java/org/dainst/redirector/Controller.java
+++ src/main/java/org/dainst/redirector/Controller.java
@@ -20,18 +20,18 @@ class Controller {
             ) {
 
         get( "/drupal/", (req,res) -> {
-            if (!req.queryParams("q").contains("node")) return withContactInfo(res,contactInfo);
+            if (!req.queryParams("q").contains("node")) return with404(res,contactInfo);
             String nodeNumber = req.queryParams("q").split("node\\/")[1];
-            if (redirectMappings.get(nodeNumber) == null) return withContactInfo(res,contactInfo);
+            if (redirectMappings.get(nodeNumber) == null) return with404(res,contactInfo);
             String redirectUrlPartial = redirectMappings.get(nodeNumber);
-            if (redirectUrlPartial == null) return withContactInfo(res,contactInfo);
+            if (redirectUrlPartial == null) return with404(res,contactInfo);
             res.redirect(targetUrl+redirectUrlPartial, 301);
             return "ok";
         });
 
         get( "/drupal/node/:node_id", (req,res) -> {
             String redirectUrlPartial = redirectMappings.get(req.params(":node_id"));
-            if (redirectUrlPartial == null) return withContactInfo(res,contactInfo);
+            if (redirectUrlPartial == null) return with404(res,contactInfo);
             res.redirect(targetUrl+redirectMappings.get(req.params(":node_id")), 301);
             return "ok";
         });
@@ -39,20 +39,20 @@ class Controller {
         get( "/item/*/:id_in_category", (req,res) -> {
             String category = req.splat()[0];
             String eid = dao.getEntityID(category,req.params(":id_in_category"));
-            if (eid == null) return withContactInfo(res,contactInfo);
+            if (eid == null) return with404(res,contactInfo);
             res.redirect(targetUrl+"entity/"+eid, 301);
             return "ok";
         });
 
         get( "/books/:book_alias", (req,res) -> {
             String eid = dao.getEntityID(req.params(":book_alias"));
-            if (eid == null) return withContactInfo(res,contactInfo);
+            if (eid == null) return with404(res,contactInfo);
             res.redirect(targetUrl+"entity/"+eid, 301);
             return "ok";
         });
     }
 
-    private String withContactInfo(Response res,String contactInfo) {
+    private String with404(Response res, String contactInfo) {
         res.status(404);
         return ("<h1>The requested URL was not found on this server. "+
                 "(Die angeforderte URL wurde auf diesem Server nicht gefunden.)</h1>"+
